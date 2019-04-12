ea2584f2c952813b2f38e12afd1d6170f278e0ce
Nash R <nramos@NAHUELs-MacBook-Air.local>
2019-04-11 19:45:01 -0500

    001 Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+// App.vue
+<template>
+  <div id="app">
+    <ProfeForm />
+  </div>
+</template>
+
+<script>
+import ProfeForm from './components/ProfeForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProfeForm
+  }
+}
+</script>
